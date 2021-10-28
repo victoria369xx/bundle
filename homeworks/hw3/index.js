adca23ce9c05ddd42773b85cbea9bb14393d72ec
Victoria <v.shurkalina@gmail.com>
2021-10-28 18:27:02 +0400

    stage 3

--- homeworks/hw3/index.js
+++ homeworks/hw3/index.js
@@ -1,1 +1,18 @@
 import './style.scss'
+
+const imgCards = [
+    {img:'./images/forrest.jpeg', title:'Лес'},
+    {img:'./images/lake.jpg', title:'Озеро'},
+    {img:'./images/mountains.jpg', title:'Горы'}
+  ];
+  const renderImgCard = (img, title) => `<div class="card">
+  <img src="${img}" alt="фото">
+  <h3>${title}</h3>
+  </div>`;
+
+  const renderImgCardsList = list => {
+    let  imgCardsList = list.map(card => renderImgCard(card.img, card.title));
+    document.querySelector('.cards').innerHTML = imgCardsList.join('');
+  }
+
+  renderImgCardsList(imgCards);
