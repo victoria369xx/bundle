06fbd3a41fadcb648bb7064cf09b7b2b138cafba
Victoria <v.shurkalina@gmail.com>
2021-10-29 13:49:01 +0400

    stage 3 edited

--- homeworks/hw3/src/image.js
+++ homeworks/hw3/src/image.js
@@ -1,6 +1,6 @@
-import img1Src from '../src/assets/images/forrest.jpeg'
-import img2Src from '../src/assets/images/lake.jpg'
-import img3Src from '../src/assets/images/mountains.jpg'
+import img1Src from './assets/images/forrest.jpeg'
+import img2Src from './assets/images/lake.jpg'
+import img3Src from './assets/images/mountains.jpg'
 
 let cards = document.querySelector('.cards');
 
@@ -10,25 +10,14 @@ let items = [
     {name: 'Горы', src: img3Src},
 ]
 
-items.forEach(item =>{
-    cards.insertAdjacentElement('beforeend', `
-    <div class="card">
+
+items.forEach((item) =>{
+    cards.insertAdjacentElement('beforeend', 
+    `<div class="card">
   <img src='${item.src}' alt="фото">
   <h3>${item.name}</h3>
   </div>`
     )
-})
+});
 
-/*
-  const renderImgCard = (img, title) => `<div class="card">
-  <img src='../src/assets/images/${img}' alt="фото">
-  <h3>${title}</h3>
-  </div>`;
-
-  const renderImgCardsList = list => {
-    let  imgCardsList = list.map(card => renderImgCard(card.image, card.title));
-    document.querySelector('.cards').innerHTML = imgCardsList.join('');
-  }
-
-  renderImgCardsList(imgCards);
-*/
+module.exports = './image.js'
